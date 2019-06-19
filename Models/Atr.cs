f8fac4768216746d30950b916f8c952e2d51c893
Martinus Suherman <martinus.suherman@gmail.com>
2019-06-19 12:11:50 +0700

    Update Atr agar tidak error saat save

--- Models/Atr.cs
+++ Models/Atr.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MonevAtr.Models
 {
@@ -17,31 +16,32 @@ namespace MonevAtr.Models
         [Key]
         public int Kode { get; set; }
 
-        [MaxLength(255)]
+        [Required, MaxLength(255)]
         public string Nama { get; set; }
 
-        [Display(Name = "Provinsi")]
-        public int KodeProvinsi { get; set; }
-
-        [Display(Name = "Kabupaten/Kota")]
-        public int KodeKabupatenKota { get; set; }
-
-        [MaxLength(50)]
+        [Required, MaxLength(50)]
         public string Nomor { get; set; }
 
-        [Display(Name = "Jenis ATR")]
-        public int KodeJenisAtr { get; set; }
+        public DateTime? Tanggal { get; set; }
 
-        public DateTime Tanggal { get; set; }
-
-        [Display(Name = "Progress")]
-        public int KodeProgressAtr { get; set; }
-
+        [Required]
         public string Aoi { get; set; }
 
         public int Luas { get; set; }
 
         public short Tahun { get; set; }
+
+        [Display(Name = "Jenis ATR")]
+        public int KodeJenisAtr { get; set; }
+
+        [Display(Name = "Progress")]
+        public int? KodeProgressAtr { get; set; }
+
+        [Display(Name = "Kabupaten/Kota")]
+        public int? KodeKabupatenKota { get; set; }
+
+        [Display(Name = "Provinsi")]
+        public int? KodeProvinsi { get; set; }
 
         [ForeignKey("KodeJenisAtr")]
         public JenisAtr JenisAtr { get; set; }
