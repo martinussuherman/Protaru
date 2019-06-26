a688be6f3a197dde766d7f44316ee8e6b3312a1c
Martinus Suherman <martinus.suherman@gmail.com>
2019-06-26 13:59:38 +0700

    rtrw regular

---
+++ Pages/RtrwRegular/Create.cshtml.cs
@@ -0,0 +1,56 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MonevAtr.Models;
+
+namespace MonevAtr.Pages.RtrwRegular
+{
+    public class CreateModel : PageModel
+    {
+        public CreateModel(MonevAtrDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.Atr Atr { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ViewData["ProgressAtr"] = await _context.GetSelectListProgressRtrwRegular();
+            ViewData["KabupatenKota"] = _context.EmptySelectListKabupatenKota;
+            ViewData["Provinsi"] = await _context.GetSelectListProvinsi();
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (this.Atr.KodeProvinsi == 0)
+            {
+                this.Atr.KodeProvinsi = null;
+            }
+
+            if (this.Atr.KodeKabupatenKota == 0)
+            {
+                this.Atr.KodeKabupatenKota = null;
+            }
+
+            this.Atr.KodeJenisAtr = (int) JenisAtrEnum.RtrwRegular;
+
+            if (!ModelState.IsValid)
+            {
+                return await OnGetAsync();
+            }
+
+            _context.Atr.Attach(this.Atr);
+            _context.Entry(this.Atr).State = EntityState.Added;
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+
+        private readonly MonevAtrDbContext _context;
+    }
+}
---
+++ Pages/RtrwRegular/Index.cshtml.cs
@@ -0,0 +1,56 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MonevAtr.Models;
+
+namespace MonevAtr.Pages.RtrwRegular
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(MonevAtrDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Models.AtrSearch AtrSearch { get; set; }
+
+        public IList<Models.ProgressAtr> ProgressAtr
+        {
+            get
+            {
+                return (from progressAtr in _context.ProgressAtr where progressAtr.KodeJenisAtr == (int) JenisAtrEnum.RtrwRegular select progressAtr).ToList();
+            }
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ViewData["Provinsi"] = await _context.GetSelectListProvinsi();
+            ViewData["KabupatenKota"] = _context.EmptySelectListKabupatenKota;
+            ViewData["Tahun"] = GetSelectListTahun();
+            return Page();
+        }
+
+        private SelectList GetSelectListTahun()
+        {
+            List<short> list = (from atr in _context.Atr where atr.KodeJenisAtr == (int) JenisAtrEnum.RtrwRegular select atr.Tahun)
+                .Distinct()
+                .ToList();
+
+            List<Models.Tahun> listHasil = new List<Tahun>();
+            listHasil.Insert(0, new Tahun(0, "Pilih Tahun"));
+
+            foreach (short tahun in list)
+            {
+                listHasil.Add(new Tahun(tahun));
+            }
+
+            return new SelectList(listHasil, "Value", "Text");
+        }
+
+        private readonly MonevAtrDbContext _context;
+    }
+}
