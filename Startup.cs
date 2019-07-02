ed6f93fad8daeb7558d4c69ca8e5246bfb8e5199
Martinus Suherman <martinus.suherman@gmail.com>
2019-07-02 18:04:48 +0700

    Tambah konfigurasi folder upload

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,15 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.FileProviders;
 
 namespace MonevAtr
 {
@@ -56,8 +56,13 @@ namespace MonevAtr
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(Path.Combine(env.WebRootPath, "upload")),
+                    RequestPath = new PathString("/upload")
+            });
 
+            app.UseCookiePolicy();
             app.UseMvc();
         }
     }
