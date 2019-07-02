f8482f3dea6ea9a3dab1d2a5d549dd8c40dc310e
Martinus Suherman <martinus.suherman@gmail.com>
2019-07-02 18:02:04 +0700

    Update RDTR

--- Pages/Rdtr/Edit.cshtml.cs
+++ Pages/Rdtr/Edit.cshtml.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -11,9 +14,10 @@ namespace MonevAtr.Pages.Rdtr
 {
     public class EditModel : PageModel
     {
-        public EditModel(MonevAtrDbContext context)
+        public EditModel(MonevAtrDbContext context, IHostingEnvironment environment)
         {
             _context = context;
+            hostingEnvironment = environment;
         }
 
         [BindProperty]
@@ -21,6 +25,9 @@ namespace MonevAtr.Pages.Rdtr
 
         [BindProperty]
         public List<Models.AtrDokumen> AtrDokumenList { get; set; }
+
+        // [BindProperty]
+        // public List<IFormFile> UploadFile { get; set; } = new List<IFormFile>();
 
         public List<Models.KelompokDokumen> KelompokDokumenList { get; set; }
 
@@ -48,15 +55,15 @@ namespace MonevAtr.Pages.Rdtr
 
         public async Task<IActionResult> OnPostAsync()
         {
-            if (!ModelState.IsValid)
-            {
-                return await OnGetAsync(this.Atr.Kode);
-            }
+            // if (!ModelState.IsValid)
+            // {
+            //     return await OnGetAsync(this.Atr.Kode);
+            // }
 
-            if (!await SaveAtr())
-            {
-                return NotFound();
-            }
+            // FixUploadFiles(HttpContext);
+
+            dokumenList = await _context.Dokumen
+                .ToListAsync();
 
             foreach (Models.AtrDokumen dokumen in this.AtrDokumenList)
             {
@@ -66,7 +73,33 @@ namespace MonevAtr.Pages.Rdtr
                 }
             }
 
+            // for (int index = 0; index < this.AtrDokumenList.Count; index++)
+            // {
+            //     if (!await SaveAtrDokumen(index))
+            //     {
+            //         return NotFound();
+            //     }
+            // }
+
+            if (!await SaveAtr())
+            {
+                return NotFound();
+            }
+
             return await OnGetAsync(this.Atr.Kode);
+        }
+
+        private void FixUploadFiles(HttpContext httpContext)
+        {
+            int count = 0;
+
+            foreach (Models.AtrDokumen dokumen in this.AtrDokumenList)
+            {
+                string propertyName = $"AtrDokumen[{count++}].UploadFile";
+                // dokumen.UploadFile = httpContext.Request.Form.Files
+                //     .Where(i => propertyName.Equals(i.Name, StringComparison.InvariantCultureIgnoreCase))
+                //     .FirstOrDefault();
+            }
         }
 
         private async void MergeAtrDokumenDenganKelompokDokumen(int? id)
@@ -78,20 +111,25 @@ namespace MonevAtr.Pages.Rdtr
             {
                 foreach (Models.Dokumen dokumen in kelompokDokumen.Dokumen)
                 {
-                    Models.AtrDokumen joinedItem = atrDokumenList.Find(k => k.KodeDokumen == dokumen.Kode);
-
-                    if (joinedItem == null)
-                    {
-                        joinedItem = new AtrDokumen();
-                        joinedItem.KodeAtr = this.Atr.Kode;
-                        joinedItem.KodeDokumen = dokumen.Kode;
-                    }
-
-                    joinedItem.Atr = this.Atr;
-                    joinedItem.Dokumen = dokumen;
-                    dokumen.DetailDokumen = joinedItem;
+                    AdjustItemProperties(dokumen);
                 }
             }
+        }
+
+        private void AdjustItemProperties(Models.Dokumen dokumen)
+        {
+            Models.AtrDokumen joinedItem = atrDokumenList.Find(k => k.KodeDokumen == dokumen.Kode);
+
+            if (joinedItem == null)
+            {
+                joinedItem = new AtrDokumen();
+                joinedItem.KodeAtr = this.Atr.Kode;
+                joinedItem.KodeDokumen = dokumen.Kode;
+            }
+
+            joinedItem.Atr = this.Atr;
+            joinedItem.Dokumen = dokumen;
+            dokumen.DetailDokumen = joinedItem;
         }
 
         private async Task<bool> SaveAtr()
@@ -117,11 +155,24 @@ namespace MonevAtr.Pages.Rdtr
             return true;
         }
 
-        private async Task<bool> SaveAtrDokumen(AtrDokumen dokumen)
+        // private async Task<bool> SaveAtrDokumen(int index)
+        // {
+        //     CopyUploadedFile(index);
+        //     return await SaveAtrDokumen(this.AtrDokumenList[index]);
+        // }
+
+        private async Task<bool> SaveAtrDokumen(Models.AtrDokumen dokumen)
         {
             if (!dokumen.StatusAda)
             {
                 return true;
+            }
+
+            Models.Dokumen tabelDokumen = dokumenList.Find(d => d.Kode == dokumen.KodeDokumen);
+
+            if (tabelDokumen.AmbilNomor == 1)
+            {
+                this.Atr.Nomor = dokumen.Nomor;
             }
 
             if (dokumen.Kode == 0)
@@ -152,6 +203,48 @@ namespace MonevAtr.Pages.Rdtr
             return true;
         }
 
+        private async void CopyUploadedFile(Models.AtrDokumen dokumen)
+        {
+            // IFormFile file = dokumen.UploadFile;
+            IFormFile file = dokumen.UploadFile.File;
+
+            if (String.IsNullOrEmpty(file.FileName) || file.Length == 0)
+            {
+                return;
+            }
+
+            string filePath = Path.Combine(hostingEnvironment.WebRootPath, "upload", file.FileName);
+            dokumen.FilePath = file.FileName;
+
+            using(FileStream stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+        }
+
+        // private async void CopyUploadedFile(int index)
+        // {
+        //     if (index >= this.UploadFile.Count)
+        //     {
+        //         return;
+        //     }
+
+        //     IFormFile file = this.UploadFile[index];
+
+        //     if (String.IsNullOrEmpty(file.FileName) || file.Length == 0)
+        //     {
+        //         return;
+        //     }
+
+        //     string filePath = Path.Combine(hostingEnvironment.WebRootPath, "upload", file.FileName);
+        //     this.AtrDokumenList[index].FilePath = file.FileName;
+
+        //     using(FileStream stream = new FileStream(filePath, FileMode.Create))
+        //     {
+        //         await file.CopyToAsync(stream);
+        //     }
+        // }
+
         private bool AtrExists(int kode)
         {
             return _context.Atr.Any(e => e.Kode == kode);
@@ -164,6 +257,10 @@ namespace MonevAtr.Pages.Rdtr
 
         private List<Models.AtrDokumen> atrDokumenList;
 
+        private List<Models.Dokumen> dokumenList;
+
         private readonly MonevAtrDbContext _context;
+
+        private readonly IHostingEnvironment hostingEnvironment;
     }
 }
--- Pages/Rdtr/EditResult.cshtml.cs
+++ Pages/Rdtr/EditResult.cshtml.cs
@@ -1,4 +1,7 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MonevAtr.Models;
@@ -18,9 +21,26 @@ namespace MonevAtr.Pages.Rdtr
         [BindProperty]
         public List<Models.AtrDokumen> AtrDokumenList { get; set; }
 
+        // [BindProperty]
+        // public List<IFormFile> UploadFile { get; set; }
+
         public IActionResult OnPost()
         {
+            FixUploadFiles(HttpContext);
             return Page();
+        }
+
+        private void FixUploadFiles(HttpContext httpContext)
+        {
+            int count = 0;
+
+            foreach (Models.AtrDokumen dokumen in this.AtrDokumenList)
+            {
+                string propertyName = $"AtrDokumen[{count++}].UploadFile";
+                // dokumen.UploadFile = httpContext.Request.Form.Files
+                //     .Where(i => propertyName.Equals(i.Name, StringComparison.InvariantCultureIgnoreCase))
+                //     .FirstOrDefault();
+            }
         }
 
         private readonly MonevAtrDbContext _context;
