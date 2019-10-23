82d11d3df559f3c43aa7865e7ebfbcd57219bee4
Martinus Suherman <martinus.suherman@gmail.com>
2019-10-23 15:34:48 +0700

    RtrwnT52 WIP

---
+++ Pages/RTrwnT52/Index.cshtml.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MonevAtr.Models;
+
+namespace MonevAtr.Pages.RtrwnT52
+{
+    public class IndexModel : PageModel
+    {
+        public IndexModel(MonevAtrDbContext context)
+        {
+            _context = context;
+            selectListUtilities = new SelectListUtilities(context);
+        }
+
+        [BindProperty]
+        public AtrSearch Rtr { get; set; }
+
+        public IList<Models.ProgressAtr> ProgressAtr
+        {
+            get
+            {
+                return _context.ProgressAtr
+                    .Where(p => p.KodeJenisAtr == (int) JenisRtrEnum.RtrwnT52)
+                    .OrderBy(p => p.Nomor)
+                    .ToList();
+            }
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            ViewData["Tahun"] = await selectListUtilities.TahunPerpresRtrwnT52();
+            return Page();
+        }
+
+        private readonly SelectListUtilities selectListUtilities;
+
+        private readonly MonevAtrDbContext _context;
+    }
+}
