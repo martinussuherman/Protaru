0eddcbfc637a04095aba3ff08751b7149f1eecbf
Martinus Suherman <martinus.suherman@gmail.com>
2019-11-19 10:49:37 +0700

    Update Index Rtr Daerah dan Nasional

---
+++ Pages/RtrIndex.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MonevAtr.Models;
+
+namespace MonevAtr.Pages
+{
+    public class RtrIndexModel : PageModel
+    {
+        public RtrIndexModel(MonevAtrDbContext context)
+        {
+            selectListUtilities = new SelectListUtilities(context);
+        }
+
+        public int StatusYear { get; set; }
+
+        public int StatusMonth { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            StatusYear = DateTime.Today.Year;
+            StatusMonth = DateTime.Today.Month;
+            ViewData["TahunLintasSektorDanPersetujuanSubstansi"] =
+                await selectListUtilities.TahunRapatLintasSektorDanPersetujuanSubstansi();
+            return Page();
+        }
+
+        private readonly SelectListUtilities selectListUtilities;
+    }
+}
