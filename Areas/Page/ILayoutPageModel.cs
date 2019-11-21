6a7db43b7811c6bd144e71f18c79c9dc78dea862
Martinus Suherman <martinus.suherman@gmail.com>
2019-11-21 16:53:10 +0700

    Add custom PageModel

---
+++ Areas/Page/ILayoutPageModel.cs
@@ -0,0 +1,7 @@
+namespace Protaru.Areas.Page
+{
+    public interface ILayoutPageModel
+    {
+        string Layout { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Protaru.Areas.Page;
+
+namespace MonevAtr.Pages
+{
+    public class IndexModel : ProtaruPageModel
+    {
+        public void OnGet()
+        {
+            Title = "Kementerian Agraria dan Tata Ruang";
+            ActiveMenu = ActiveMenu.Home;
+        }
+    }
+}
