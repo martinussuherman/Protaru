255646dca9bfd7e85c77fe02ba9da31049ea13e1
Martinus Suherman <martinus.suherman@gmail.com>
2020-02-04 14:48:18 +0700

    update custom page model

--- Areas/Page/ProtaruPageModel.cs
+++ Areas/Page/ProtaruPageModel.cs
@@ -1,15 +1,11 @@
 using System;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+using Itm.Misc;
 
 namespace Protaru.Areas.Page
 {
-    public class ProtaruPageModel : PageModel, ITitlePageModel, ILayoutPageModel, IActiveMenuPageModel
+    public class ProtaruPageModel : CustomPageModel, ILayoutPageModel, IActiveMenuPageModel
     {
         public string Layout { get; set; } = String.Empty;
-
-        public string PageTitle { get; set; } = String.Empty;
-
-        public string Title { get; set; } = String.Empty;
 
         public ActiveMenu ActiveMenu { get; set; }
     }
