a139a3fb3dc5a9cf42959beac43b2831c1febe10
Martinus Suherman <martinus.suherman@gmail.com>
2020-08-22 14:30:47 +0700

    Update geo data

--- wwwroot/markercluster.js
+++ wwwroot/markercluster.js
@@ -1,14 +1,26 @@
-[
+﻿[
     {
-        "latitude": -6.9051,
-        "longitude": 107.6081,
+        "latitude": -6.9225,
+        "longitude": 107.6267,
         "location": "Kota Bandung",
         "rtr": "RTRW Kota Bandung",
         "progress": "Peninjauan Kembali",
-        "rekomGub": "",
+        "rekomGub": "650/4882/Bapp",
         "linSek": "",
         "perSub": "",
-        "perda": "1 Tahun 2013",
+        "perda": "18 Tahun 2011",
         "link": "https://atr.wordpress-theme.bid/protaru/RtrwT51/View/299?returnUrl=%2FRtrIndexDaerah"
+    },
+    {
+        "latitude": -3.32,
+        "longitude": 114.5908,
+        "location": "Kota Banjarmasin",
+        "rtr": "RTRW Kota Banjarmasin",
+        "progress": "Perda",
+        "rekomGub": "650/559/ITR/Bapp",
+        "linSek": "",
+        "perSub": "",
+        "perda": "15 Tahun 2013",
+        "link": "https://atr.wordpress-theme.bid/protaru/RtrwT51/View/404?returnUrl=%2FRtrIndexDaerah"
     }
 ]
