c260d7572cfe93b55ef6804b26d89424e9e3b170
Martinus Suherman <martinus.suherman@gmail.com>
2020-09-03 00:34:42 +0700

    Add progress filter

--- Pages/Rtrw/Progress.cshtml.cs
+++ Pages/Rtrw/Progress.cshtml.cs
@@ -1,6 +1,9 @@
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using MonevAtr.Models;
 
 namespace MonevAtr.Pages.Rtrw
@@ -14,6 +17,20 @@ namespace MonevAtr.Pages.Rtrw
         }
 
         public AtrSearch Rtr { get; set; }
+
+        public IList<Models.ProgressAtr> Progress
+        {
+            get
+            {
+                return _context.ProgressAtr
+                    .Where(p =>
+                        p.KodeJenisAtr == (int)JenisRtrEnum.RtrwT51 ||
+                        p.KodeJenisAtr == (int)JenisRtrEnum.RtrwT52)
+                    .OrderBy(p => p.Nomor)
+                    .AsNoTracking()
+                    .ToList();
+            }
+        }
 
         public async Task<IActionResult> OnGetAsync()
         {
