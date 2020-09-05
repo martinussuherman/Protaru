174c26fd78fc3bce56cd56322e0714017d157222
Martinus Suherman <martinus.suherman@gmail.com>
2020-09-05 23:41:31 +0700

    Add autocomplete search

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Itm.Misc;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MonevAtr.Models;
+using Syncfusion.EJ2.DropDowns;
 
 namespace MonevAtr.Pages
 {
     public class IndexModel : CustomPageModel
     {
-        public void OnGet()
+        public IndexModel(
+            PomeloDbContext context)
         {
-            Title = "Kementerian Agraria dan Tata Ruang";
+            _context = context;
         }
+
+        public List<string> NamaRtr { get; set; }
+
+        public AutoCompleteFieldSettings FieldSettings => new AutoCompleteFieldSettings
+        {
+            Text = "Nama",
+            Value = "Nama"
+        };
+
+        public async Task<IActionResult> OnGet()
+        {
+            NamaRtr = await _context.Atr
+                .OrderBy(c => c.Nama)
+                .AsNoTracking()
+                .Select(c => c.Nama )
+                .Distinct()
+                .ToListAsync();
+            Title = "Kementerian Agraria dan Tata Ruang";
+            return Page();
+        }
+
+        private readonly PomeloDbContext _context;
     }
 }
