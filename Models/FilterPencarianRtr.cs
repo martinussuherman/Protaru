dd5d51325ef4f250f11b1e37c16514ece29d70f8
Martinus Suherman <martinus.suherman@gmail.com>
2020-11-04 12:58:20 +0700

    Update model for view

--- Models/FilterPencarianRtr.cs
+++ Models/FilterPencarianRtr.cs
@@ -1,10 +1,5 @@
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
 namespace MonevAtr.Models
 {
-    [Table("filter_pencarian_rtr")]
     public class FilterPencarianRtr
     {
         public int Kode { get; set; }
