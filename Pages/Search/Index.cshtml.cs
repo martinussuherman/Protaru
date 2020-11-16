ca8b780a6e52f0a7bb4fff3219c9e56f855b0ad0
Martinus Suherman <martinus.suherman@gmail.com>
2020-11-17 00:11:53 +0700

    Sort tahun

--- Pages/Search/Index.cshtml.cs
+++ Pages/Search/Index.cshtml.cs
@@ -30,90 +30,33 @@ namespace MonevAtr.Pages.Search
             .AsNoTracking()
             .ToList();
 
-        public IList<int> TahunRekomendasiGubernur => _context.AtrDokumen
-            .ByKodeList(FilterUtilitiesExtensions.KodeDokumenRekomendasiGubernur)
-            .OrderBy(a => a.Tanggal.Year)
-            .AsNoTracking()
-            .Select(a => a.Tanggal.Year)
-            .Distinct()
-            .Where(y => y > 1)
-            .ToList();
+        public IList<int> TahunRekomendasiGubernur { get; set; }
 
-        public IList<int> TahunPermohonanPersetujuanSubstansi => _context.AtrDokumen
-            .ByKodeList(FilterUtilitiesExtensions.KodeDokumenPermohonanPersetujuanSubstansi)
-            .OrderBy(a => a.Tanggal.Year)
-            .AsNoTracking()
-            .Select(a => a.Tanggal.Year)
-            .Distinct()
-            .Where(y => y > 1)
-            .ToList();
+        public IList<int> TahunPermohonanPersetujuanSubstansi { get; set; }
 
-        public IList<int> TahunMasukLoket => _context.AtrDokumen
-            .ByKodeList(FilterUtilitiesExtensions.KodeDokumenMasukLoket)
-            .OrderBy(a => a.Tanggal.Year)
-            .AsNoTracking()
-            .Select(a => a.Tanggal.Year)
-            .Distinct()
-            .Where(y => y > 1)
-            .ToList();
+        public IList<int> TahunMasukLoket { get; set; }
 
-        public IList<int> TahunRapatLintasSektor => _context.AtrDokumen
-            .ByKodeList(FilterUtilitiesExtensions.KodeDokumenRapatLintasSektor)
-            .OrderBy(a => a.Tanggal.Year)
-            .AsNoTracking()
-            .Select(a => a.Tanggal.Year)
-            .Distinct()
-            .Where(y => y > 1)
-            .ToList();
+        public IList<int> TahunRapatLintasSektor { get; set; }
 
-        public IList<int> TahunPersetujuanSubstansi => _context.AtrDokumen
-            .ByKodeList(FilterUtilitiesExtensions.KodeDokumenPersetujuanSubstansi)
-            .OrderBy(a => a.Tanggal.Year)
-            .AsNoTracking()
-            .Select(a => a.Tanggal.Year)
-            .Distinct()
-            .Where(y => y > 1)
-            .ToList();
+        public IList<int> TahunPersetujuanSubstansi { get; set; }
 
-        public IList<int> TahunPerda
-        {
-            get
-            {
-                List<short> list = _context.Atr
-                    .OrderBy(a => a.Tahun)
-                    .AsNoTracking()
-                    .Select(a => a.Tahun)
-                    .Distinct()
-                    .Where(y => y > 1)
-                    .ToList();
-
-                List<int> result = new List<int>();
-
-                foreach (short value in list)
-                {
-                    result.Add(value);
-                }
-
-                return result;
-            }
-        }
-
-        // public IList<Models.Dokumen> Dokumen
-        // {
-        //     get
-        //     {
-        //         return _context.Dokumen
-        //             .Include(p => p.KelompokDokumen)
-        //             .Include(p => p.KelompokDokumen.JenisAtr)
-        //             .OrderBy(p => p.KelompokDokumen.KodeJenisAtr)
-        //             .ThenBy(p => p.KelompokDokumen.Nomor)
-        //             .ThenBy(p => p.Nomor)
-        //             .ToList();
-        //     }
-        // }
+        public IList<int> TahunPerda { get; set; }
 
         public async Task<IActionResult> OnGetAsync()
         {
+            TahunRekomendasiGubernur =
+                await selectListUtilities.TahunRekomendasiGubernurListAsync();
+            TahunPermohonanPersetujuanSubstansi =
+                await selectListUtilities.TahunPermohonanPersetujuanSubstansiListAsync();
+            TahunMasukLoket =
+                await selectListUtilities.TahunMasukLoketListAsync();
+            TahunRapatLintasSektor =
+                await selectListUtilities.TahunRapatLintasSektorListAsync();
+            TahunPersetujuanSubstansi =
+                await selectListUtilities.TahunPersetujuanSubstansiListAsync();
+            TahunPerda =
+                await selectListUtilities.TahunPerdaAsync(JenisRtrEnum.All);
+
             ViewData["Provinsi"] = await selectListUtilities.Provinsi();
             ViewData["KabupatenKota"] = selectListUtilities.EmptyKabupatenKota;
             return Page();
