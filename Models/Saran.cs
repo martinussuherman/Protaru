28b3011f6588ab0df848e641c128eb6a37b1dd84
Martinus Suherman <martinus.suherman@gmail.com>
2020-11-26 00:32:09 +0700

    Update models

---
+++ Models/Saran.cs
@@ -0,0 +1,10 @@
+﻿namespace Protaru.Models
+{
+    public partial class Saran
+    {
+        public uint Id { get; set; }
+        public string Nama { get; set; }
+        public string Email { get; set; }
+        public string Isi { get; set; }
+    }
+}
