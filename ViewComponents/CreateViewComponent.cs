c8c0c238a3676570fdab32ff6b491b72d516d5c6
Martinus Suherman <martinus.suherman@gmail.com>
2020-11-27 01:33:37 +0700

    Update create rtr view component

--- ViewComponents/CreateViewComponent.cs
+++ ViewComponents/CreateViewComponent.cs
@@ -10,7 +10,9 @@ namespace Protaru.ViewComponents.Rtr
             return View(new ViewModel
             {
                 Rtr = rtr,
-                DisplayAoi = displayAoi
+                DisplayAoi = displayAoi,
+                Location = LocationByRtr((JenisRtrEnum)rtr.KodeJenisAtr),
+                ReadOnlyLocation = ReadOnlyByRtr((JenisRtrEnum)rtr.KodeJenisAtr)
             });
         }
 
@@ -19,6 +21,48 @@ namespace Protaru.ViewComponents.Rtr
             public Atr Rtr { get; set; }
 
             public bool DisplayAoi { get; set; }
+
+            public RtrLocationEnum Location { get; set; }
+
+            public bool ReadOnlyLocation { get; set; }
+        }
+
+        private bool ReadOnlyByRtr(JenisRtrEnum jenisRtr)
+        {
+            if (jenisRtr == JenisRtrEnum.RdtrT51 ||
+                jenisRtr == JenisRtrEnum.RtrKpnT51 ||
+                jenisRtr == JenisRtrEnum.RtrKsnT51 ||
+                jenisRtr == JenisRtrEnum.RtrPulauT51 ||
+                jenisRtr == JenisRtrEnum.RtrwnT51 ||
+                jenisRtr == JenisRtrEnum.RtrwT50 ||
+                jenisRtr == JenisRtrEnum.RtrwT51)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        private RtrLocationEnum LocationByRtr(JenisRtrEnum jenisRtr)
+        {
+            if (jenisRtr == JenisRtrEnum.RtrwnT51 ||
+                jenisRtr == JenisRtrEnum.RtrwnT52)
+            {
+                return RtrLocationEnum.None;
+            }
+
+            if (jenisRtr == JenisRtrEnum.RtrKsnT51 ||
+                jenisRtr == JenisRtrEnum.RtrKsnT52)
+            {
+                return RtrLocationEnum.Kawasan;
+            }
+
+            if (jenisRtr == JenisRtrEnum.RtrPulauT51 ||
+                jenisRtr == JenisRtrEnum.RtrPulauT52)
+            {
+                return RtrLocationEnum.Pulau;
+            }
+
+            return RtrLocationEnum.ProvinsiKabKota;
         }
     }
 }
