cfd643869ae875abcbdd410c8e99e154ca1d9e86
Martinus Suherman <martinus.suherman@gmail.com>
2020-12-02 12:21:31 +0700

    Refactor and remove unused code

--- Helper/SelectListUtilities.cs
+++ Helper/SelectListUtilities.cs
@@ -16,16 +16,6 @@ namespace MonevAtr.Models
             _context = context;
         }
 
-        public SelectList EmptyKabupatenKota
-        {
-            get
-            {
-                IList<KabupatenKota> list = new List<KabupatenKota>();
-                InsertPilihKabupatenKota(list);
-                return new SelectList(list, "Kode", "Nama");
-            }
-        }
-
         public IEnumerable<SelectListItem> StatusRegularItems(int? value = 0)
         {
             int val = value == null ? 0 : value.Value;
@@ -149,10 +139,6 @@ namespace MonevAtr.Models
 
             return list.Prepend(_pulauTitle);
         }
-        public async Task<SelectList> Pulau()
-        {
-            return new SelectList(await PulauAsync(), _valueProperty, _textProperty);
-        }
 
         public async Task<IEnumerable<SelectListItem>> KawasanAsync()
         {
@@ -168,10 +154,6 @@ namespace MonevAtr.Models
                 .ToListAsync();
 
             return list.Prepend(_kawasanTitle);
-        }
-        public async Task<SelectList> Kawasan()
-        {
-            return new SelectList(await KawasanAsync(), _valueProperty, _textProperty);
         }
 
         public async Task<List<int>> TahunRekomendasiGubernurListAsync()
@@ -428,7 +410,7 @@ namespace MonevAtr.Models
 
         public async Task<IEnumerable<SelectListItem>> ProgressRtrAsync(
             int jenisRtr,
-            int? value = 0)
+            int? value)
         {
             int val = value == null ? 0 : value.Value;
             IEnumerable<SelectListItem> temp = (await ProgressRtrAsync(jenisRtr))
@@ -441,12 +423,16 @@ namespace MonevAtr.Models
 
             return temp.Prepend(_rtrProgressTitle);
         }
-        private async Task<IList<ProgressAtr>> ProgressRtrAsync(int jenisRtr)
+        public async Task<IList<ProgressAtr>> ProgressRtrAsync(int jenisRtr)
         {
-            return await _context.ProgressAtr
-                .Where(p => p.KodeJenisAtr == jenisRtr)
+            IQueryable<ProgressAtr> query = _context.ProgressAtr.AsNoTracking();
+
+            if (jenisRtr != (int)JenisRtrEnum.None)
+            {
+                query = query.Where(p => p.KodeJenisAtr == jenisRtr);
+            }
+            return await query
                 .OrderBy(p => p.Nomor)
-                .AsNoTracking()
                 .ToListAsync();
         }
         private async Task<SelectList> TahunPerda(JenisRtrEnum jenis)
@@ -478,51 +464,9 @@ namespace MonevAtr.Models
                 listHasil.Add(new Tahun(tahun));
             }
         }
-        private void InsertPilihKabupatenKota(IList<KabupatenKota> list)
-        {
-            KabupatenKota pilih = new KabupatenKota
-            {
-                Kode = 0,
-                Nama = "Pilih Kabupaten/Kota"
-            };
-
-            list.Insert(0, pilih);
-        }
         private void UpdateNamaDokumen(Dokumen dokumen)
         {
             dokumen.Nama = dokumen.Nama + " - " + dokumen.KelompokDokumen.JenisAtr.Nama;
-        }
-        private async Task UpdateNamaKawasan(List<Kawasan> list)
-        {
-            List<KawasanProvinsi> listProvinsi = await _context.KawasanProvinsi
-                .Include(q => q.Provinsi)
-                .OrderBy(q => q.KodeKawasan)
-                .ThenBy(q => q.Provinsi.Nama)
-                .AsNoTracking()
-                .ToListAsync();
-
-            StringBuilder builder = new StringBuilder();
-
-            foreach (Kawasan kawasan in list)
-            {
-                builder.Clear();
-                builder.Append(kawasan.Nama);
-                builder.Append(" - ");
-
-                List<KawasanProvinsi> match = listProvinsi.FindAll(q => q.KodeKawasan == kawasan.Kode);
-
-                for (int index = 0; index < match.Count; index++)
-                {
-                    if (index > 0)
-                    {
-                        builder.Append(", ");
-                    }
-
-                    builder.Append(match[index].Provinsi.Nama);
-                }
-
-                kawasan.Nama = builder.ToString();
-            }
         }
         private IEnumerable<SelectListItem> InputTahun(int value)
         {
@@ -548,9 +492,9 @@ namespace MonevAtr.Models
             StatusRevisi.RevisiT54
         };
         private static readonly SelectListItem _regularTitle =
-            new SelectListItem("Pilih Status RTR T5-0/T5-1", string.Empty);
+            new SelectListItem("Pilih Status RTR Lima tahun pertama", string.Empty);
         private static readonly SelectListItem _revisiTitle =
-            new SelectListItem("Pilih Status RTR T5-2", string.Empty);
+            new SelectListItem("Pilih Status RTR Lima tahun kedua", string.Empty);
         private static readonly SelectListItem _rtrProgressTitle =
             new SelectListItem("Pilih Progress RTR", string.Empty);
         private static readonly SelectListItem _provinsiTitle =
