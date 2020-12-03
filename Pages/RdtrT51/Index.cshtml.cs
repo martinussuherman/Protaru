2afaa7776f0b6a564148fae90d2b42be3f7aae74
Martinus Suherman <martinus.suherman@gmail.com>
2020-12-03 09:23:35 +0700

    Change from partial view to view component

--- Pages/RdtrT51/Index.cshtml.cs
+++ Pages/RdtrT51/Index.cshtml.cs
@@ -1,9 +1,6 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using MonevAtr.Models;
 
 namespace MonevAtr.Pages.RdtrT51
@@ -12,24 +9,11 @@ namespace MonevAtr.Pages.RdtrT51
     {
         public IndexModel(PomeloDbContext context)
         {
-            _context = context;
             selectListUtilities = new SelectListUtilities(context);
         }
 
         [BindProperty]
         public AtrSearch Rtr { get; set; }
-
-        public IList<Models.ProgressAtr> ProgressAtr
-        {
-            get
-            {
-                return _context.ProgressAtr
-                    .Where(p => p.KodeJenisAtr == (int)JenisRtrEnum.RdtrT51)
-                    .OrderBy(p => p.Nomor)
-                    .AsNoTracking()
-                    .ToList();
-            }
-        }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -38,7 +22,5 @@ namespace MonevAtr.Pages.RdtrT51
         }
 
         private readonly SelectListUtilities selectListUtilities;
-
-        private readonly PomeloDbContext _context;
     }
 }
--- Pages/RtrPulauT51/Index.cshtml.cs
+++ Pages/RtrPulauT51/Index.cshtml.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -11,23 +9,11 @@ namespace MonevAtr.Pages.RtrPulauT51
     {
         public IndexModel(PomeloDbContext context)
         {
-            _context = context;
             selectListUtilities = new SelectListUtilities(context);
         }
 
         [BindProperty]
         public AtrSearch Rtr { get; set; }
-
-        public IList<Models.ProgressAtr> ProgressAtr
-        {
-            get
-            {
-                return _context.ProgressAtr
-                    .Where(p => p.KodeJenisAtr == (int)JenisRtrEnum.RtrPulauT51)
-                    .OrderBy(p => p.Nomor)
-                    .ToList();
-            }
-        }
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -36,7 +22,5 @@ namespace MonevAtr.Pages.RtrPulauT51
         }
 
         private readonly SelectListUtilities selectListUtilities;
-
-        private readonly PomeloDbContext _context;
     }
 }
