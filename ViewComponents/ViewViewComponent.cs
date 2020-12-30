0a43869c04f9a87cfabc3666d232efcc63556409
Martinus Suherman <martinus.suherman@gmail.com>
2020-12-30 19:34:59 +0700

    Rtr view title, button, status and nomor

--- ViewComponents/ViewViewComponent.cs
+++ ViewComponents/ViewViewComponent.cs
@@ -12,7 +12,9 @@ namespace Protaru.ViewComponents.Rtr
             {
                 Rtr = detail,
                 PreviousPage = PreviousPageName(jenis),
-                NextPage = NextPageName(jenis)
+                NextPage = NextPageName(jenis),
+                Title = TitleByJenis(detail.Rtr, jenis),
+                StatusNomor = StatusNomor(detail.Rtr)
             });
         }
 
@@ -21,8 +23,38 @@ namespace Protaru.ViewComponents.Rtr
             public RtrDetail Rtr { get; set; }
             public string PreviousPage { get; set; }
             public string NextPage { get; set; }
+            public string Title { get; set; }
+            public string StatusNomor { get; set; }
         }
 
+        private string StatusNomor(Atr rtr)
+        {
+            if(string.IsNullOrEmpty(rtr.Nomor) )
+            {
+                return rtr.DisplayNamaProgress;
+            }
+
+            return $"{rtr.DisplayNamaProgress} Nomor: {rtr.Nomor}";
+        }
+        private string TitleByJenis(Atr rtr, JenisRtrEnum jenis)
+        {
+            switch (jenis)
+            {
+                case JenisRtrEnum.RtrwnT51:
+                case JenisRtrEnum.RtrwnT52:
+                    return string.Empty;
+
+                case JenisRtrEnum.RtrPulauT51:
+                case JenisRtrEnum.RtrPulauT52:
+                    return rtr.DisplayNamaPulau;
+
+                case JenisRtrEnum.RtrKsnT51:
+                case JenisRtrEnum.RtrKsnT52:
+                    return rtr.DisplayNamaKawasan;
+            }
+
+            return rtr.DisplayNamaProvinsi;
+        }
         private string PreviousPageName(JenisRtrEnum jenis)
         {
             return ((JenisRtrEnum)((int)jenis - 1)).ToString();
